ae8ca260d3aab150a8952a0bc9b3fde22ecf0f84
Vinko Mlacic <vinko@chino.io>
2023-02-05 21:27:08 +0100

    Add better favicon
    Refs: #VMW-4

--- local_deployment/build.ps1
+++
@@ -1,4 +0,0 @@
-# Builds the Docker image for the website
-# Run this from the repository folder: ./local_deployment/build.ps1
-docker build -t website:latest -f ./local_deployment/Dockerfile .
-Write-Output "Successfully build Docker image for website."
--- local_deployment/start.ps1
+++
@@ -1,5 +0,0 @@
-# Starts the container which holds the website. When the container stops,
-# it will be deleted.
-# NOTE: run this from the repository root folder: ./local_deployment/start.ps1
-docker run --rm --name website -p 8080:80 -d website
-Write-Output "Website successfully started and running at http://localhost:8080"
